9232321207aaa3568cdacff0d292422767be3f36
Andrew Harris <harris.1305@gmail.com>
2017-03-03 12:14:53 -0500

    add sorting scores by timestamp desc

--- src/DataHelper.php
+++ src/DataHelper.php
@@ -39,6 +39,14 @@ class DataHelper {
 						)
 					);
 				}
+				// sort scores by timestamp desc
+				$sortScores = static function($a, $b) {
+					if ($a === $b) {
+						return 0;
+					}
+					return $a < $b ? 1 : -1;
+				};
+				usort($page->scores, $sortScores);
 				array_push($pages, $page);
 			}
 
