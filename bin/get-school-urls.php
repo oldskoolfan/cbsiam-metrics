0628daa76422394c6e6a88134129f5f8803725b6
Andrew Harris <harris.1305@gmail.com>
2017-03-13 09:59:24 -0400

    code documentation

--- bin/get-school-urls.php
+++ bin/get-school-urls.php
@@ -8,6 +8,7 @@
  * 1. flush the redis cache
  * 2. scrape the cbsiam partner site for school urls
  * 3. load those as keys in redis
+ * 4. get results from pagespeed api, save to redis
  */
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -21,6 +22,8 @@ use Symfony\Component\DomCrawler\Crawler;
 try {
 	$dataHelper = new DataHelper();
 	$scraper = new Scraper();
+
+	echo "getting school links...\n";
 	$crawler = $scraper->getSchoolListPage();
 
 	$linkElements = $crawler->filter('div.work-link a')->each(
@@ -30,8 +33,10 @@ try {
 	);
 
 	// flush database
+	echo "flushing database...\n";
 	$dataHelper->redis->flushDb();
 
+	echo "saving school links...\n";
 	foreach ($linkElements as $link) {
 		$num = $dataHelper->redis->sAdd('schoolUrls', $link);
 	}
--- src/HttpHelper.php
+++ src/HttpHelper.php
@@ -8,17 +8,35 @@ use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Exception\ClientException;
 
 class HttpHelper {
-
+	/**
+	 * @var string
+	 */
 	const PAGESPEED_URL = 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed';
 
+	/**
+	 * @var GuzzleHttp\Pool
+	 */
 	public $pool;
 
+	/**
+	 * @var GuzzleHttp\Client
+	 */
 	private $client;
 
+	/**
+	 * @var DataHelper
+	 */
 	private $dataHelper;
 
+	/**
+	 * @var array
+	 */
 	private $links;
 
+	/**
+	 * @param array      $links
+	 * @param DataHelper $dataHelper
+	 */
 	public function __construct(array $links, DataHelper &$dataHelper) {
 		$this->client = new Client();
 		$this->dataHelper = $dataHelper;
@@ -26,6 +44,12 @@ class HttpHelper {
 		$this->generateRequests($links);
 	}
 
+	/**
+	 * Do the heavy lifting: create a promise pool
+	 * firing 5 requests at a time to the pagespeed api
+	 * @param  array  $links
+	 * @return void
+	 */
 	private function generateRequests(array $links) {
 		$promises = function ($links) {
 			foreach ($links as $link) {
@@ -52,6 +76,13 @@ class HttpHelper {
 		]);
 	}
 
+	/**
+	 * persist score data to redis via dataHelper
+	 * @param  string 	$url
+	 * @param  string 	$scoreKey
+	 * @param  stdClass $response
+	 * @return void
+	 */
 	private function saveScoreData($url, $scoreKey, $response) {
 		$scoreData = [
 			'speedScore' => $response->ruleGroups->SPEED->score,
@@ -64,14 +95,21 @@ class HttpHelper {
 		$this->dataHelper->savePageScoreData($url, $scoreKey, $scoreData);
 	}
 
+	/**
+	 * persist rule data to redis via dataHelper
+	 * @param  string 	$scoreKey
+	 * @param  stdClass $response
+	 * @return void
+	 */
 	private function saveRuleData($scoreKey, $response) {
 		$ruleData = [];
 
 		foreach($response->formattedResults->ruleResults as $key => $val) {
 			$ruleData[$key . ':name'] = $val->localizedRuleName;
 			$ruleData[$key . ':impact'] = $val->ruleImpact;
-			$rules = new PageRuleCollection($scoreKey, $ruleData);
-			$this->dataHelper->savePageRuleData($rules);
 		}
+
+		$rules = new PageRuleCollection($scoreKey, $ruleData);
+		$this->dataHelper->savePageRuleData($rules);
 	}
 }
